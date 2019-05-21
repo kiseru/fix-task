fdb71c6e31443f9db278503e9c1a1c47ffb5a005
Alexandr Kiselev <kis.alex97@gmail.com>
2019-05-21 15:38:08 +0300

    Added HorseService interface

--- src/main/java/com/kiseru/fix/fixtask/services/HorseService.java
+++ src/main/java/com/kiseru/fix/fixtask/services/HorseService.java
@@ -1,52 +1,5 @@
 package com.kiseru.fix.fixtask.services;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Map;
-import java.util.regex.Pattern;
-
-@Service
-public class HorseService {
-
-    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final Pattern LETTERS_PATTERN = Pattern.compile("[A-Z]+");
-    private static final Pattern NUMBERS_PATTERN = Pattern.compile("[0-9]+");
-    private static final Pattern POSITION_PATTERN = Pattern.compile("^[A-Z]+[0-9]+$");
-
-    public int getMovesCount(int fieldHeight, int fieldWidth, String startPosition, String endPosition) {
-        return 0;
-    }
-
-    private Map.Entry<Integer, Integer> convertPosition(String position) {
-        if (!POSITION_PATTERN.matcher(position).matches()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-
-        var letterMatcher = LETTERS_PATTERN.matcher(position);
-        letterMatcher.find();
-        var col = (convertLettersToInt(letterMatcher.group()));
-
-        var numberMatcher = NUMBERS_PATTERN.matcher(position);
-        numberMatcher.find();
-        var row = Integer.valueOf(numberMatcher.group()) - 1;
-
-        return Map.entry(col, row);
-    }
-
-    private int convertLettersToInt(String letters) {
-        if (!LETTERS_PATTERN.matcher(letters).matches()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-
-        var result = 0;
-        for (int i = 0; i < letters.length(); i++) {
-            var c = letters.charAt(i);
-            var charIndex = ALPHABET.indexOf(c);
-            result = ALPHABET.length() * result + charIndex;
-        }
-
-        return result;
-    }
+public interface HorseService {
+    int getMovesCount(int fieldHeight, int fieldWidth, String startPosition, String endPosition);
 }
--- src/test/java/com/kiseru/fix/fixtask/services/impl/HorseServiceImplTest.java
+++ src/test/java/com/kiseru/fix/fixtask/services/impl/HorseServiceImplTest.java
@@ -1,5 +1,6 @@
-package com.kiseru.fix.fixtask.services;
+package com.kiseru.fix.fixtask.services.impl;
 
+import com.kiseru.fix.fixtask.services.HorseService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +14,7 @@ import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HorseServiceTest {
+public class HorseServiceImplTest {
 
     @Autowired
     private HorseService horseService;
