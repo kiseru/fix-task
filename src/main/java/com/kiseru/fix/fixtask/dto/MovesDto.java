dffd149dbaead6b097e7950bf8d1dedddcedace6
Alexandr Kiselev <kis.alex97@gmail.com>
2019-05-21 13:15:38 +0300

    Added servlet endpoint

---
+++ src/main/java/com/kiseru/fix/fixtask/dto/MovesDto.java
@@ -0,0 +1,12 @@
+package com.kiseru.fix.fixtask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MovesDto {
+    private int moves;
+}
