3bd430be433842340de84631ef089ef5e312dca5
Alexandr Kiselev <kis.alex97@gmail.com>
2019-05-21 13:05:07 +0300

    Initial commit

---
+++ src/main/java/com/kiseru/fix/fixtask/FixTaskApplication.java
@@ -0,0 +1,12 @@
+package com.kiseru.fix.fixtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kiseru/fix/fixtask/FixTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kiseru.fix.fixtask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FixTaskApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
